e8add051f55abfb40ee7d5fc993386d0f0945952
Theogene <manamfashatheogene@gmail.com>
2021-10-28 20:28:14 +0200

    commit initial

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using System.Data.Common; 
+using System.Data;
+using System.Data.SqlClient;
+
+namespace manamfasha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+           
+
+
+        }
+    }
+}
